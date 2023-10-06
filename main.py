e9855d1f8426fa3151ab94f59e5dd96c380fea6b
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2023-10-06 17:27:17 +0700

    done similarities_file

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import codecs
 def similarities_file(file1, file2):
     s = read_file(file1)
     p = read_file(file2)
-    print(s)
-    print(p)
+    similarities =  similarities_string(s, p)
+    print(similarities)
 
+# Hàm tìm chuỗi con chung dài nhất
 def similarities_string(string1, string2):
     string1 = "x" + string1
     string2 = "x" + string2
@@ -15,8 +16,8 @@ def similarities_string(string1, string2):
                 a[i][j] = a[i - 1][j - 1] + 1
             else:
                 a[i][j] = max(a[i - 1][j], a[i][j - 1])
-    # print(a[len(string1) - 1][len(string2) - 1])
-    print(a)
+    max_str = a[len(string1) - 1][len(string2) - 1]
+    return ((max_str / (len(string1) - 1)) + (max_str / (len(string2) - 1))) / 2
 
 def read_file(path):
     # Đọc file bằng mã hóa UTF-8
@@ -25,6 +26,5 @@ def read_file(path):
 
     return java_code
 
-similarities_string('cabah', 'cha')
 
-# similarities_file('C:\\Users\HAIZUKA\\java\kkk\\src\model\\Student.java', 'C:\\Users\HAIZUKA\\java\kkk\\src\model\\People.java')
+similarities_file('C:\\Users\HAIZUKA\\java\kkk\\src\model\\Student.java', 'C:\\Users\HAIZUKA\\java\kkk\\src\model\\People.java')
