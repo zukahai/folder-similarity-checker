a5cb1945c3f0a71e9f9c03aed997ecceb35f10e6
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2023-10-07 08:57:30 +0700

    done similaritie_folder

--- main.py
+++ main.py
@@ -3,11 +3,38 @@ from gitignore_parser import parse_gitignore
 import os
 class HaiZuka:
     @staticmethod
+    def similarities_folder(folder1, folder2):
+        HaiZuka.try_similarities(folder1, folder2)
+        
+    @staticmethod
+    def try_similarities(folder1, folder2):
+        list_files = HaiZuka.read_folder(folder1)
+        print(list_files)
+        for file in list_files:
+            if HaiZuka.is_path_ignored(file):
+                continue
+            # kiểm tra file có phải 1 file hay không
+            if os.path.isfile(folder1 + '/' + file) and os.path.isfile(folder2 + '/' + file):
+                print("File: ", file)
+                similaritie = HaiZuka.similarities_file(folder1 + '/' + file, folder2 + '/' + file)
+                print(similaritie)
+            # kiểm tra file có phải 1 folder không
+            elif os.path.isdir(folder1 + '/' + file) and os.path.isdir(folder2 + '/' + file):
+                print("Folder: ", file)
+                HaiZuka.try_similarities(folder1 + '/' + file, folder2 + '/' + file)
+
+
+    @staticmethod
+    def read_folder(folder):
+        s = os.listdir(folder)
+        return s
+
+    @staticmethod
     def similarities_file(file1, file2):
         s = HaiZuka.read_file(file1)
         p = HaiZuka.read_file(file2)
         similarities =  HaiZuka.similarities_string(s, p)
-        print(similarities)
+        return similarities
 
     # Hàm tìm chuỗi con chung dài nhất
     @staticmethod
@@ -34,10 +61,7 @@ class HaiZuka:
     @staticmethod
     def is_path_ignored(path_to_check, gitignore_file='ignore.txt'):
         gitignore = parse_gitignore(gitignore_file)
-
-        print(gitignore)
-
         return gitignore(path_to_check)
 
 
-HaiZuka.similarities_file('C:\\Users\HAIZUKA\\java\kkk\\src\model\\People.java', 'C:\\Users\HAIZUKA\\java\DHDN\\src\model\\People.java')
+HaiZuka.similarities_folder('C:\\Users\HAIZUKA\\java\kkk', 'C:\\Users\HAIZUKA\\java\DHDN')
