00387bde0f7b644768ab7604e54338778904030e
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2023-11-08 11:24:09 +0700

    update theme

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ class App:
     def __init__(self, root):
         self.root = root
         self.root.title("Kiểm tra thư mục tương đồng | HaiZuka")
-        self.root.geometry("600x570")
-        # self.root.resizable(False, False)
-        self.root.geometry("600x570+{}+{}".format(self.root.winfo_screenwidth() // 2 - 600 // 2, self.root.winfo_screenheight() // 2 - 600 // 2))
+        self.root.geometry("657x570")
+        self.root.resizable(False, False)
+        self.root.geometry("657x570+{}+{}".format(self.root.winfo_screenwidth() // 2 - 657 // 2, self.root.winfo_screenheight() // 2 - 600 // 2))
 
         self.style = ThemedStyle(self.root)
         self.style.set_theme("scidblue")
@@ -80,6 +80,7 @@ class App:
         self.frame1 = tk.Frame(self.root)
         self.frame1.configure(bg="#333333")
         self.frame1.grid(row=1, column=0, columnspan=4, padx=10, pady=1)
+        self.frame1.grid_columnconfigure(0, weight=1)
         
         button_font = (font_roboto, 12)
         button_bg_color = "#776B5D"
@@ -89,15 +90,15 @@ class App:
         button_borderwidth = 2
         button_relief = "groove"  # Loại border
 
-        self.button1 = tk.Button(self.frame1, text="Chọn Folder 1", command=self.select_folder1, font=button_font, bg=button_bg_color, fg=button_fg_color, borderwidth=button_borderwidth, relief=button_relief, image=select_icon, compound="left")
+        self.button1 = tk.Button(self.frame1, text="Select Folder 1", command=self.select_folder1, font=button_font, bg=button_bg_color, fg=button_fg_color, borderwidth=button_borderwidth, relief=button_relief, image=select_icon, compound="left")
         self.button1.image = select_icon  # Giữ tham chiếu đến biểu tượng
         self.button1.grid(row=0, column=0, padx=10, pady=10)
 
-        self.button2 = tk.Button(self.frame1, text="Chọn Folder 2", command=self.select_folder2, font=button_font, bg=button_bg_color, fg=button_fg_color, borderwidth=button_borderwidth, relief=button_relief, image=select_icon, compound="left")
+        self.button2 = tk.Button(self.frame1, text="Select Folder 2", command=self.select_folder2, font=button_font, bg=button_bg_color, fg=button_fg_color, borderwidth=button_borderwidth, relief=button_relief, image=select_icon, compound="left")
         self.button2.image = select_icon  # Giữ tham chiếu đến biểu tượng
         self.button2.grid(row=0, column=1, padx=10, pady=10)
 
-        self.compare_button = tk.Button(self.frame1, text="So sánh từng cặp", command=self.compare_pairs, font=button_font, bg=button_bg_color, fg=button_fg_color, borderwidth=button_borderwidth, relief=button_relief, image=compare_icon, compound="left")
+        self.compare_button = tk.Button(self.frame1, text="Compare each pair", command=self.compare_pairs, font=button_font, bg=button_bg_color, fg=button_fg_color, borderwidth=button_borderwidth, relief=button_relief, image=compare_icon, compound="left")
         self.compare_button.image = compare_icon  # Giữ tham chiếu đến biểu tượng
         self.compare_button.grid(row=0, column=2, padx=10, pady=10)
 
@@ -105,21 +106,22 @@ class App:
         self.frame2 = tk.Frame(self.root)
         self.frame2.configure(bg="#333333")
         self.frame2.grid(row=2, column=0, columnspan=4, padx=10, pady=1)
+        self.frame2.grid_columnconfigure(0, weight=1)
 
-        self.result_button = tk.Button(self.frame2, text="Hiển Thị Kết Quả", command=self.display_paths, font=button_font, bg=button_bg_color, fg=button_fg_color, borderwidth=button_borderwidth, relief=button_relief, image=result_icon, compound="left")
+        self.result_button = tk.Button(self.frame2, text="Display Results", command=self.display_paths, font=button_font, bg=button_bg_color, fg=button_fg_color, borderwidth=button_borderwidth, relief=button_relief, image=result_icon, compound="left")
         self.result_button.image = result_icon  # Giữ tham chiếu đến biểu tượng
         self.result_button.grid(row=0, column=0, padx=10, pady=10)
         self.result_button.config(state=tk.DISABLED)
 
-        self.refresh_button = tk.Button(self.frame2, text="Làm mới", command=self.refresh, font=button_font, bg=button_bg_color, fg=button_fg_color, borderwidth=button_borderwidth, relief=button_relief, image=refesh_icon, compound="left")
+        self.refresh_button = tk.Button(self.frame2, text="Refresh", command=self.refresh, font=button_font, bg=button_bg_color, fg=button_fg_color, borderwidth=button_borderwidth, relief=button_relief, image=refesh_icon, compound="left")
         self.refresh_button.image = refesh_icon  # Giữ tham chiếu đến biểu tượng
         self.refresh_button.grid(row=0, column=1, padx=10, pady=10)
 
-        self.theme_button = tk.Button(self.frame2, text="Đổi Theme", command=self.toggle_theme, font=button_font, bg=button_bg_color, fg=button_fg_color, borderwidth=button_borderwidth, relief=button_relief, image=change_theme_icon, compound="left")
+        self.theme_button = tk.Button(self.frame2, text="Change Theme", command=self.toggle_theme, font=button_font, bg=button_bg_color, fg=button_fg_color, borderwidth=button_borderwidth, relief=button_relief, image=change_theme_icon, compound="left")
         self.theme_button.image = change_theme_icon  # Giữ tham chiếu đến biểu tượng
         self.theme_button.grid(row=0, column=2, padx=10, pady=10)
 
-        self.open_folder_button = tk.Button(self.frame2, text="File kết quả", command=self.open_folder, font=button_font, bg=button_bg_color, fg=button_fg_color, borderwidth=button_borderwidth, relief=button_relief, image=folder_result_icon, compound="left")
+        self.open_folder_button = tk.Button(self.frame2, text="Results Container", command=self.open_folder, font=button_font, bg=button_bg_color, fg=button_fg_color, borderwidth=button_borderwidth, relief=button_relief, image=folder_result_icon, compound="left")
         self.open_folder_button.image = folder_result_icon  # Giữ tham chiếu đến biểu tượng
         self.open_folder_button.grid(row=0, column=3, padx=10, pady=10)
 
@@ -134,10 +136,10 @@ class App:
     def update_ui(self):
         self.index_theme += 1
         self.index_theme %= len(self.theme)
-        bg = self.theme[self.index_theme]["bg"]
+        bg = self.theme[self.index_theme]["bg_button"]
         fg = self.theme[self.index_theme]["fg"]
         text_area_bg = self.theme[self.index_theme]["textarea"]
-        frame_bg = self.theme[self.index_theme]["frame"]
+        frame_bg = self.theme[self.index_theme]["bg"]
         self.frame1.configure(bg=frame_bg)
         self.frame2.configure(bg=frame_bg)
         self.root.configure(bg=frame_bg)
@@ -152,6 +154,12 @@ class App:
         self.open_folder_button.configure(bg=bg, fg=fg)
 
         Util.write_json_utf8("theme.json", {'index': self.index_theme, 'themes': self.theme})
+        try:
+            current_text = self.text_area.get("1.0", "end-1c")
+            json_data = json.loads(current_text)
+        except:
+            json_data = self.template_json
+        self.set_textarea(json_data)
 
     def select_folder1(self):
         self.folder1_path = filedialog.askdirectory()
@@ -218,27 +226,26 @@ class App:
         
         self.text_area.tag_configure("red_text", foreground="red")
         self.text_area.tag_configure("bracket_text", foreground="#34ebde")
-        self.text_area.tag_configure("dot_text", foreground="#f95c8f")
-        self.text_area.tag_configure("red_text", foreground="red")
+        self.text_area.tag_configure("true_text", foreground=self.theme[self.index_theme]["fg_textarea_1"])
+        self.text_area.tag_configure("false_text", foreground=self.theme[self.index_theme]["fg_textarea_2"])
         self.text_area.tag_configure("yellow_text", foreground="yellow")
-
-
         self.text_area.tag_configure("custom_bold", font=("font_roboto", 10, "bold"))
+        
         flag = False
         for ch in str(formatted_json):
-            if ch in ['{', '}']:
-                self.text_area.insert("insert", ch, "custom_bold bracket_text")
+            if ch in ['{', '}', ':']:
+                self.text_area.insert("insert", ch, "custom_bold false_text")
             elif ch in ['/', '[', ']']:
-                self.text_area.insert("insert", ch, "custom_bold yellow_text")
+                self.text_area.insert("insert", ch, "custom_bold true_text")
             else:
                 if ch == '"':
                     flag = not flag
-                    self.text_area.insert("insert", ch, "custom_bold red_text")
+                    self.text_area.insert("insert", ch, "custom_bold fg_text")
                 else:
                     if flag:
-                        self.text_area.insert("insert", ch, "custom_bold dot_text")
+                        self.text_area.insert("insert", ch, "custom_bold true_text")
                     else:
-                        self.text_area.insert("insert", ch, "custom_bold")
+                        self.text_area.insert("insert", ch, "custom_bold false_text")
     
     def disable_buttons(self):
         self.button1.config(state=tk.NORMAL)
